d622f54424756b29687dd49749dcd3504172083c
SarajiMalathi <105904966+SarajiMalathi@users.noreply.github.com>
2022-05-20 12:01:35 +0530

    Add files via upload

---
+++ IneDre.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+public class IneDre {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=++a;
+		System.out.println("a"+a);
+		System.out.println("b"+b);
+		
+
+	}
+
+}
