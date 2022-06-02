d073d4a32510123e5decf18c3d36b3fc68308b51
SarajiMalathi <105904966+SarajiMalathi@users.noreply.github.com>
2022-06-03 00:12:23 +0530

    Add files via upload

---
+++ LargestThreeNumbers.java
@@ -0,0 +1,22 @@
+package com.edu;
+
+public class LargestThreeNumbers {
+	
+	public int largestNumberThree(int n1,int n2,int n3) {
+		int l=(n1>n2&&n1>n3)?n1:(n2>n1&&n2>n3)?n2:n3;
+		return l;
+		
+	}
+}
+
+
+
+
+	
+		
+	
+
+
+
+
+
